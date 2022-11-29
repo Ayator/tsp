7c2ed248cb979e8398805c68c2fc9ff23ce7bf7a
Ayator <victor.d.ayala.2001@gmail.com>
2022-11-29 08:45:54 -0500

    First Submission

--- src/Main.java
+++ src/Main.java
@@ -9,26 +9,31 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
+        String filename = "tsp_8_1";
+        System.out.println("Running " + filename);
+        TravelingSalesmanProblem(filename);
+        System.out.println("");
+        runAll();
+    }
+
+    public static void runAll(){
         LinkedHashMap<String, Float> goals = getGoals();
-        LinkedHashMap<String, Boolean> goalsBeaten = getGoalsBeaten(goals);
+        // LinkedHashMap<String, Boolean> goalsBeaten = getGoalsBeaten(goals);
         for (String filename : goals.keySet()) {
-            int n = 10;
-            // if(goalsBeaten.get(filename)){
-            //     n = 1;
-            // }
-            // else{
-            //     n = 1;
+            int n = 1;
+            // if(!goalsBeaten.get(filename)){
+            //     n = 100;
             // }
 
             for (int i = 0; i < n; i++) {
                 System.out.println("Running " + filename);
-                runTSP(filename);
+                TravelingSalesmanProblem(filename);
                 System.out.println("");
             }
         }
     }
 
-    public static void runTSP(String filename){
+    public static void TravelingSalesmanProblem(String filename){
         String inputPath = "../input/" + filename;
         String outputPath = "../output/" + filename;
         String bestOutputPath = "../bestOutput/" + filename;
@@ -69,9 +74,8 @@ public class Main{
             // TwoOpt.Apply(hamiltonianCycle);
             TwoOpt.SimulatedAnnealing(hamiltonianCycle);
             System.out.println("TwoOpt done.");
-            if(!"tsp_3038_1 tsp_3795_1 tsp_4461_1 tsp_5915_1 tsp_5934_1 tsp_7397_1 tsp_9432_1 tsp_11849_1 tsp_14051_1".contains(filename)){
-                // 3-opt may be too slow for these
-                // haven't tested how long yet
+            if(!"tsp_2103_1 tsp_2152_1 tsp_2319_1 tsp_2392_1 tsp_3038_1 tsp_3795_1 tsp_4461_1 tsp_5915_1 tsp_5934_1 tsp_7397_1 tsp_9432_1 tsp_11849_1 tsp_14051_1".contains(filename)){
+                // non optimized 3-opt may be too slow for these
                 ThreeOpt.Apply(hamiltonianCycle);
                 System.out.println("ThreeOpt done.");
             }
@@ -111,6 +115,7 @@ public class Main{
         }
     }
 
+    // ignore this~
     public static LinkedHashMap<String, Boolean> getGoalsBeaten(LinkedHashMap<String, Float> goals){
         LinkedHashMap<String, Boolean> goalsBeaten = new LinkedHashMap<>(77);
         try {
@@ -140,58 +145,57 @@ public class Main{
         return goalsBeaten;
     }
 
+    // greedy stuff
     public static LinkedHashMap<String, Float> getGoals(){
         LinkedHashMap<String, Float> goals = new LinkedHashMap<>(77);
-        // how is this even possible
-        // goals.put("tsp_2392_1", 378063f);
         
-        // goals.put("tsp_5_1", 4f);
-        // goals.put("tsp_8_1", 13.9508f);
-        // goals.put("tsp_51_1", 437.577f);
-        // goals.put("tsp_70_1", 693.704f);
-        // goals.put("tsp_76_1", 563.911f);
-        // goals.put("tsp_76_2", 110542f);
-        // goals.put("tsp_99_1", 1257.74f);
-        // goals.put("tsp_100_1", 22013.3f);
-        // goals.put("tsp_100_2", 22334.4f);
-        // goals.put("tsp_100_3", 21995.3f);
-        // goals.put("tsp_100_4", 21743.5f);
-        // goals.put("tsp_100_5", 22876.8f);
-        // goals.put("tsp_100_6", 8347.1f);
-        // goals.put("tsp_101_1", 658.708f);
-        // goals.put("tsp_105_1", 14869.2f);
-        // goals.put("tsp_107_1", 45229.8f);
-        // goals.put("tsp_124_1", 61238.1f);
-        // goals.put("tsp_127_1", 126883f);
-        // goals.put("tsp_136_1", 103478f);
-        // goals.put("tsp_144_1", 61179.7f);
-        // goals.put("tsp_150_1", 27176.1f);
-        // goals.put("tsp_150_2", 26956f);
-        // goals.put("tsp_152_1", 74255.8f);
-        // goals.put("tsp_159_1", 43375.7f);
-        // goals.put("tsp_195_1", 2435.67f);
-        // goals.put("tsp_198_1", 16515.8f);
-        // goals.put("tsp_200_1", 31541.7f);
-        // goals.put("tsp_200_2", 30559.7f);
-        // goals.put("tsp_225_1", 135918f);
-        // goals.put("tsp_226_1", 87935.5f);
-        // goals.put("tsp_262_1", 2504.23f);
-        // goals.put("tsp_264_1", 56142.4f);
-        // goals.put("tsp_299_1", 49906f);
-        // goals.put("tsp_318_1", 45047.3f);
-        // goals.put("tsp_318_2", 44395f);
-        // goals.put("tsp_400_1", 16249.1f);
-        // goals.put("tsp_417_1", 12972.4f);
-        // goals.put("tsp_439_1", 117150f);
-        // goals.put("tsp_442_1", 54041.1f);
-        // goals.put("tsp_493_1", 37343.9f);
-        // goals.put("tsp_574_1", 40000.1f);
-        // goals.put("tsp_575_1", 7292.92f);
-        // goals.put("tsp_654_1", 39607.2f);
-        // goals.put("tsp_657_1", 52477.6f);
-        // goals.put("tsp_724_1", 44981.4f);
-        // goals.put("tsp_783_1", 9660.55f);
-        // goals.put("tsp_1000_1", 20647700f);
+        goals.put("tsp_5_1", 4f);
+        goals.put("tsp_8_1", 13.9508f);
+        goals.put("tsp_51_1", 437.577f);
+        goals.put("tsp_70_1", 693.704f);
+        goals.put("tsp_76_1", 563.911f);
+        goals.put("tsp_76_2", 110542f);
+        goals.put("tsp_99_1", 1257.74f);
+        goals.put("tsp_100_1", 22013.3f);
+        goals.put("tsp_100_2", 22334.4f);
+        goals.put("tsp_100_3", 21995.3f);
+        goals.put("tsp_100_4", 21743.5f);
+        goals.put("tsp_100_5", 22876.8f);
+        goals.put("tsp_100_6", 8347.1f);
+        goals.put("tsp_101_1", 658.708f);
+        goals.put("tsp_105_1", 14869.2f);
+        goals.put("tsp_107_1", 45229.8f);
+        goals.put("tsp_124_1", 61238.1f);
+        goals.put("tsp_127_1", 126883f);
+        goals.put("tsp_136_1", 103478f);
+        goals.put("tsp_144_1", 61179.7f);
+        goals.put("tsp_150_1", 27176.1f);
+        goals.put("tsp_150_2", 26956f);
+        goals.put("tsp_152_1", 74255.8f);
+        goals.put("tsp_159_1", 43375.7f);
+        goals.put("tsp_195_1", 2435.67f);
+        goals.put("tsp_198_1", 16515.8f);
+        goals.put("tsp_200_1", 31541.7f);
+        goals.put("tsp_200_2", 30559.7f);
+        goals.put("tsp_225_1", 135918f);
+        goals.put("tsp_226_1", 87935.5f);
+        goals.put("tsp_262_1", 2504.23f);
+        goals.put("tsp_264_1", 56142.4f);
+        goals.put("tsp_299_1", 49906f);
+        goals.put("tsp_318_1", 45047.3f);
+        goals.put("tsp_318_2", 44395f);
+        goals.put("tsp_400_1", 16249.1f);
+        goals.put("tsp_417_1", 12972.4f);
+        goals.put("tsp_439_1", 117150f);
+        goals.put("tsp_442_1", 54041.1f);
+        goals.put("tsp_493_1", 37343.9f);
+        goals.put("tsp_574_1", 40000.1f);
+        goals.put("tsp_575_1", 7292.92f);
+        goals.put("tsp_654_1", 39607.2f);
+        goals.put("tsp_657_1", 52477.6f);
+        goals.put("tsp_724_1", 44981.4f);
+        goals.put("tsp_783_1", 9660.55f);
+        goals.put("tsp_1000_1", 20647700f);
         goals.put("tsp_1060_1", 247086f);
         goals.put("tsp_1084_1", 263421f);
         goals.put("tsp_1173_1", 62699f);
@@ -206,24 +210,24 @@ public class Main{
         goals.put("tsp_1748_1", 378852f);
         goals.put("tsp_1817_1", 67363.4f);
         goals.put("tsp_1889_1", 377287f);
+
         goals.put("tsp_2103_1", 89860.9f);
         goals.put("tsp_2152_1", 75714f);
-        goals.put("tsp_2319_1", 261672f);
-        goals.put("tsp_2392_1", 378063f);// 3-opt works fine till here
-        
-        // goals.put("tsp_3038_1", 160642f);// 3-opt not tested
-        // goals.put("tsp_3795_1", 35093.3f);
-        // goals.put("tsp_4461_1", 211369f);
-        // goals.put("tsp_5915_1", 688904f);
-        // goals.put("tsp_5934_1", 685560f);
-        // goals.put("tsp_7397_1", 27700000f);
-        // goals.put("tsp_9432_1", 65994.6f);
-        // goals.put("tsp_11849_1", 1120000f);
-        // goals.put("tsp_14051_1", 574617f);// Christofides works fine till here (PC is crying)
+        goals.put("tsp_2319_1 ", 261672f);
+        goals.put("tsp_2392_1", 378063f);// 3-opt works fine till here // this score is unreal
+        goals.put("tsp_3038_1", 160642f);// 3-opt not tested
+        goals.put("tsp_3795_1", 35093.3f);
+        goals.put("tsp_4461_1", 211369f);
+        goals.put("tsp_5915_1", 688904f);
+        goals.put("tsp_5934_1", 685560f);
+        goals.put("tsp_7397_1", 27700000f);
+        goals.put("tsp_9432_1", 65994.6f);
+        goals.put("tsp_11849_1", 1120000f);
+        goals.put("tsp_14051_1", 574617f);// Christofides works fine till here (PC is crying)
 
-        // goals.put("tsp_18512_1", 790940f);// skip to 2-opt from the beginning (RAM)
-        // goals.put("tsp_33810_1", 78500000f);
-        // goals.put("tsp_85900_1", 163000000f);// 2-opt works for everything
+        goals.put("tsp_18512_1", 790940f);// skip to 2-opt from the beginning (RAM)
+        goals.put("tsp_33810_1", 78500000f);
+        goals.put("tsp_85900_1", 163000000f);// 2-opt works for everything
         return goals;
     }
 }
